2bfef35e96d70943e15bbfacb56540e7781727d5
Timo Salm <accounts@timosalm.de>
2025-01-30 11:45:46 +0100

    Improvements

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+- Attach cluster to Tanzu Platform with label region=epc
+- Configure Route 53 GSLB credential in Tanzu Platform (Setup & Configuration -> Credentials)
+- Add CA cert for registry to kapp-controller in the workload cluster
+```
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kapp-controller-config
+  namespace: tkg-system
+stringData:
+  caCerts: |
+    -----BEGIN CERTIFICATE-----
+    MIIIrzCCB5egAwIBAgIQApGSTwwhz9lqGp1U5fMRUDANBgkqhkiG9w0BAQsFADBP
+    MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMSkwJwYDVQQDEyBE
+    ....
+    -----END CERTIFICATE----- 
+```
+
+
 ```
 cp values-example.yaml values.yaml
 tanzu project use <my-project>
--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 set -x
 set -eo pipefail
 
-rm -rf generated && ytt -f . --output-files generated/ --ignore-unknown-comments --file-mark '*.yaml:exclusive-for-output=true'  --file-mark '*.yml:exclusive-for-output=true' --file-mark 'clustergroups/**/*:exclusive-for-output=true' --file-mark 'values-examlpe.yaml:exclude=true'
-(cd generated && tanzu deploy --nested-contexts)
+rm -rf generated && ytt -f . --output-files generated/ --ignore-unknown-comments --file-mark '*.yaml:exclusive-for-output=true'  --file-mark '*.yml:exclusive-for-output=true' --file-mark 'clustergroups/**/*:exclusive-for-output=true' --file-mark 'spaces/**/*:exclusive-for-output=true' --file-mark 'values-examlpe.yaml:exclude=true'
+(cd generated && tanzu deploy --nested-contexts -y)
